71749b324886f1fa1ea62c047e4ca7714d2d1983
batelam@post.jce.ac.il <batelam>
2022-12-25 11:51:05 +0200

    toDo list

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import { useState } from 'react';
 function App() {
 
   const [toDo, setToDo] = useState('')
+  const [toDoList, setToDoList] = useState(['Buy Milk','Go to a Movie', 'Walk the Dog', 'Watch semi final'])
 
   function handleTodoChange(e){
     setToDo(e.target.value)
   }
 
+  function AddItem(){
+    setToDoList(toDoList.push(toDo))
+  }
+
   return (
     <div className="App">
       <input value={toDo} onChange={handleTodoChange}/>
-      <button>Add to list</button>
+      <button onClick={AddItem}>Add to list</button>
+      <h3>List of things to do</h3>
+      <ul>
+        {toDoList.map(
+          toDo => (
+            <li>{toDo}</li>
+          )
+        )
+
+        }
+      </ul>
     </div>
   );
 }
